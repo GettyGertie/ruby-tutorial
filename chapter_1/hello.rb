01723def477e5771bb8c05b187731979840095fa
GettyGertie <gettyorawo@gmail.com>
2016-06-23 12:49:35 +0300

    Add instance

--- chapter_1/hello.rb
+++ chapter_1/hello.rb
@@ -6,12 +6,18 @@ name = input.chomp
 puts "Welcome, #{name}!"
 
 puts "I've got a random number between 1 and 100"
+puts "Please type a phrase"
+input = gets
+phrase = input.chomp
+puts "phrase.reverse"
 puts "Can you guess it?"
 target = rand(100) + 1
 num_guesses = 0
 guessed_it = false
-until num_guesses == 10 || guessed_it
-	puts "You've got #{10 - num_guesses} guesses left."
+until num_guesses ==10 || guessed_it
+	puts "You've got #{10-num_guesses} guesses left."
+	i = "*" * (10-num_guesses)
+	puts i
 	print "Make a guess: "
 	guess = gets.to_i
 	num_guesses += 1
