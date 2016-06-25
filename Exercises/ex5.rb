f5f350b8edc700d43a9fc8bf883101f9486fb183
GettyGertie <gettyorawo@gmail.com>
2016-06-25 21:33:46 +0300

    Learn interpolation

--- Exercises/ex5.rb
+++ Exercises/ex5.rb
@@ -4,6 +4,8 @@ my_shoe_size = 8
 food = "chips"
 guy = "Andrew"
 status = "single"
-
+drink = "water"
+drink2 = "wine"
 puts "My name is #{my_name}. I am #{my_age} years old. I really love shes so my shoesize is size number #{my_shoe_size}. 
-Favorite food will definitely have to be #{food}. I am #{status} but my guys name is #{guy}. That's all you need to know."
+Favorite food will definitely have to be #{food}. I am #{status} but my guys name is #{guy}.I drink #{drink} but sometimes
+I also take #{drink2}That's all you need to know."
