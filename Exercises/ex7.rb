b97af691c9765684d25a548e07b7215a7a640bde
GettyGertie <gettyorawo@gmail.com>
2016-06-28 11:10:53 +0300

    Completed Chapter 7

---
+++ Exercises/ex7.rb
@@ -0,0 +1,16 @@
+wool = "wool"
+puts "   baa baa black sheep" * 2 
+puts "  Have you any #{wool}." * 2
+puts "  Yes Sir" * 2 
+puts "Three bags full!"
+# what'd that do?
+
+end1 = "M"
+end2 = "y"
+end3 = "B"
+end4 = "a"
+end5 = "b"
+end6 = "y"
+
+puts end1 + end2
+puts end3 + end4 + end5 + end6
